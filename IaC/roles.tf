e93a4a710a5026b3c4e7c7b7e0e9f087b5f3ca98
dkhmely <khmelnytskyi.denys@gmail.com>
2025-04-12 22:07:06 +0200

    role for app on kv

--- IaC/roles.tf
+++ IaC/roles.tf
@@ -3,3 +3,9 @@ resource "azurerm_role_assignment" "acr_pull" {
   role_definition_name = "AcrPull"
   principal_id         = azurerm_linux_web_app.app.identity[0].principal_id
 }
+
+resource "azurerm_role_assignment" "kv_read" {
+  scope                = data.azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_linux_web_app.app.identity[0].principal_id
+}
