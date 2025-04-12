f9f53c3ee8a0d16c0530924b06f1bf2da6cf47af
dkhmely <khmelnytskyi.denys@gmail.com>
2025-04-12 21:11:13 +0200

    fix issue

--- IaC/data.tf
+++ IaC/data.tf
@@ -2,7 +2,7 @@ data "azurerm_client_config" "current" {}
 
 data "azurerm_key_vault_secret" "db_password" {
   name         = azurerm_key_vault_secret.secret.name
-  key_vault_id = azurerm_key_vault.key_vault.id
+  key_vault_id = data.azurerm_key_vault.kv.id
 }
 
 data "azurerm_resource_group" "rg" {
