2e616339059f21de164d5a852dee00752c46fdb1
dkhmely <khmelnytskyi.denys@gmail.com>
2025-04-13 12:33:44 +0200

    fix test

--- IaC/webapp.tf
+++ IaC/webapp.tf
@@ -1,8 +1,10 @@
 module "webapp" {
-  source              = "git::https://github.com/dkhmely/jti-terraform-modules.git//modules/webapp?ref=v1.0.4"
+  source              = "git::https://github.com/dkhmely/jti-terraform-modules.git//modules/webapp?ref=v1.0.5"
   name                = "${var.application}-${var.env}"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
+  acr                 = azurerm_container_registry.acr.id
+  kv                  = data.azurerm_key_vault.kv.id
 
   db_host                = "${azurerm_mysql_flexible_server.sql_server.name}.mysql.database.azure.com"
   db_user                = "${var.db_user_prefix}${var.env}user"
