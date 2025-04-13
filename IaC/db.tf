c3388f133a12ed50cb39283c4c1fb78908d61046
dkhmely <khmelnytskyi.denys@gmail.com>
2025-04-13 14:14:21 +0200

    test

--- IaC/db.tf
+++ IaC/db.tf
@@ -1,5 +1,5 @@
 module "mysql" {
-  source = "git::https://github.com/dkhmely/jti-terraform-modules.git//modules/mysql?ref=v1.0.10"
+  source = "git::https://github.com/dkhmely/jti-terraform-modules.git//modules/mysql?ref=v1.0.11"
 
   name                       = var.application
   env                        = var.env
@@ -9,8 +9,6 @@ module "mysql" {
   db_name                    = var.application
   key_vault_id               = data.azurerm_key_vault.kv.id
   vnet_id                    = data.azurerm_virtual_network.vnet.id
-  private_dns_zone_id        = azurerm_private_dns_zone.private_dns_zone.id
   private_endpoint_subnet_id = azurerm_subnet.pep_subnet.id
-  dns_zone_name              = azurerm_private_dns_zone.private_dns_zone.name
 
 }
