dd90f9a6f8ef5b761df46177c5eded5ff0c4bc7b
dkhmely <khmelnytskyi.denys@gmail.com>
2025-04-13 13:26:08 +0200

    test

--- IaC/webapp.tf
+++ IaC/webapp.tf
@@ -1,14 +1,14 @@
 module "webapp" {
-  source              = "git::https://github.com/dkhmely/jti-terraform-modules.git//modules/webapp?ref=v1.0.6"
+  source              = "git::https://github.com/dkhmely/jti-terraform-modules.git//modules/webapp?ref=v1.0.7"
   name                = "${var.application}-${var.env}"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   acr_id              = azurerm_container_registry.acr.id
   key_vault_id        = data.azurerm_key_vault.kv.id
 
-  db_host                = "${module.sql.server_name}.mysql.database.azure.com"
+  db_host                = "${module.mysql.server_name}.mysql.database.azure.com"
   db_user                = "${var.db_user_prefix}${var.env}user"
-  db_password_secret_uri = "https://${data.azurerm_key_vault.kv.name}.vault.azure.net/secrets/${azurerm_key_vault_secret.secret.name}/"
+  db_password_secret_uri = "https://${data.azurerm_key_vault.kv.name}.vault.azure.net/secrets/${module.mysql.admin_creds}/"
   db_name                = var.application
   subnet_id              = azurerm_subnet.webapp_subnet.id
   vnet_integration       = true
